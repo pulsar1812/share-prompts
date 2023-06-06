e5a8f510d5c411a0bfbb8d3213bbdabb52138c5f
pulsar1812 <cluster000@gmail.com>
2023-06-06 23:53:54 +0800

    Add database connection and User model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,25 @@
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 
+import { connectToDB } from '@/utils/database'
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: '',
-      clientSecret: '',
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB()
+
+      // Check if a user already exists
+
+      //
+    } catch (err) {}
+  },
 })
 
 export { handler as GET, handler as POST }
---
+++ utils/database.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true)
+
+  if (isConnected) {
+    console.log('MongoDB is already connected')
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_prompts',
+    })
+
+    isConnected = true
+
+    console.log('MongoDB connected')
+  } catch (err) {
+    console.log(err)
+  }
+}
