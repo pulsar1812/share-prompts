1518bcb39cc1712f3c6445f5ceb20060abb8056a
pulsar1812 <cluster000@gmail.com>
2023-06-08 23:40:00 +0800

    Add session data to Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,20 @@ import Image from 'next/image'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
 
       setProviders(response)
     }
-  })
+
+    setUpProviders()
+  }, [])
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -34,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -46,7 +48,8 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
+                // src='/assets/images/logo.svg'
                 alt='Profile'
                 width={37}
                 height={37}
@@ -73,10 +76,11 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
+              // src='/assets/images/logo.svg'
               alt='Profile'
               width={37}
               height={37}
